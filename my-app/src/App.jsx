6a312eccead9614e077a4bc4c47b091b763f9cbe
AYOUB RAHMOUN <108706030+rahmoun03@users.noreply.github.com>
2025-05-27 12:11:27 +0000

    merge succes with mouna

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import Register from './pages/register';
+import Login from './pages/login';
 
 function App() {
   return (
     <div>
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
         </Routes>
     </div>
