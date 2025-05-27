6fed0396364289ce426349834e966793712285b9
AYOUB RAHMOUN <108706030+rahmoun03@users.noreply.github.com>
2025-05-27 12:02:53 +0000

    merge succes with ilham

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import SettingsPage from './pages/Settings'
-import './App.css'
-
+import { Routes, Route, Link } from 'react-router-dom';
+import Register from './pages/register';
 
 function App() {
-
-
   return (
-    <SettingsPage />
+    <div>
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/Register" element={<Register />} />
+        </Routes>
+    </div>
   );
 }
 
