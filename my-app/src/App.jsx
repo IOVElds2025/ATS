c6060b79e355c0389765e0b8d682df1a210bbaac
Hibara-y <hibar7436@gmail.com>
2025-05-29 13:10:06 +0100

    Fix: Adjusted hero section spacing and reverted opacity changes

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import Register from './pages/register';
 import Login from './pages/login';
 import UploadResumePage from './pages/UploadResumePage';
